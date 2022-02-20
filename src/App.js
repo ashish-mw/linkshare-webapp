ebb1297098935c928e35ac3aec2464fb0233104b
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-02-20 10:56:06 +0530

    clean up and use react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// components
+import Loading from "./components/Loading";
+
+// pages
+import NotFoundPage from "./pages/NotFoundPage";
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AuthPage = lazy(() => import("./pages/AuthPage"));
+const MyLinksPage = lazy(() => import("./pages/MyLinksPage"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/my-links" element={<MyLinksPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
---
+++ src/pages/MyLinksPage.jsx
@@ -0,0 +1,5 @@
+const MyLinksPage = () => {
+  return <h1>My Links</h1>;
+};
+
+export default MyLinksPage;
