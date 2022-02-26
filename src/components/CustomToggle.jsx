3cd993152f2a951a38411e4a165d4afc66792982
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-02-26 15:09:50 +0530

    class check

--- src/components/CustomToggle.jsx
+++ src/components/CustomToggle.jsx
@@ -1,17 +1,21 @@
-import React from "react"
-import './styles/CustomToggle.css';
+import React from "react";
+import "./styles/CustomToggle.css";
 
 const CustomToggle = (props) => {
-    return(
-        <div className="skin-wrap">
-            Light mode
-            <label class="switch">
-                <input type="checkbox" checked={props.checked} onChange={props.onChange}/>
-                <span class="slider round"></span>
-            </label>
-            Dark mode
-        </div>
-    );
-}
+  return (
+    <div className="skin-wrap">
+      Light mode
+      <label className="switch">
+        <input
+          type="checkbox"
+          checked={props.checked}
+          onChange={props.onChange}
+        />
+        <span className="slider round"></span>
+      </label>
+      Dark mode
+    </div>
+  );
+};
 
 export default CustomToggle;
